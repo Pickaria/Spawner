551ab4dd2b52d0b44f8f2a993f6bedf384efb26b
Erwan <erwan.legloannec@outlook.com>
2022-12-22 16:26:50 +0100

    Add UUID setter

--- src/main/kotlin/fr/pickaria/spawner/spawnVillager.kt
+++ src/main/kotlin/fr/pickaria/spawner/spawnVillager.kt
@@ -4,12 +4,14 @@ import net.minecraft.server.level.ServerLevel
 import org.bukkit.Location
 import org.bukkit.craftbukkit.v1_19_R2.CraftWorld
 import org.bukkit.entity.Villager
+import java.util.*
 
-fun spawnVillager(location: Location, title: String): Villager {
+fun spawnVillager(location: Location, title: String, uniqueId: UUID = UUID.randomUUID()): Villager {
 	val world: ServerLevel = (location.world as CraftWorld).handle
 
 	val villager = CustomVillager(location, title)
 
+	villager.uuid = uniqueId
 	villager.setPos(location.x, location.y, location.z)
 
 	world.addFreshEntity(villager)
